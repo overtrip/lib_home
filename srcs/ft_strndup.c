fc5ee7c960b0bbf7b7573c68729711117d5ffac5
Jeremy ALONSO <jealonso@e3r12p7.42.fr>
2015-11-28 18:51:29 +0100

    correct ft_strndup.c

--- srcs/ft_strndup.c
+++ srcs/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: jealonso <jealonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 17:57:22 by jealonso          #+#    #+#             */
-/*   Updated: 2015/11/26 15:16:23 by jealonso         ###   ########.fr       */
+/*   Updated: 2015/11/28 18:51:04 by jealonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 char	*ft_strndup(const char *s1, int count)
 {
 	char	*s2;
+	int		save;
 
 	s2 = NULL;
-	if ((s2 = (char *)malloc(sizeof(char) * ++count)))
-		while (s1 && *s1 && --count)
+	save = count;
+	if ((s2 = (char *)malloc(sizeof(char) * ++save)))
+		while (s1 && *s1 && --save)
 		{
 			*s2 = *s1;
 			++s2;
